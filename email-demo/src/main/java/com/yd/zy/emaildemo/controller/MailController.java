131b6b3f71c4774a5236be080b6266d6b75ff320
zhangya <321736660@qq.com>
2019-08-19 10:12:23 +0800

    hgjghj

--- email-demo/src/main/java/com/yd/zy/emaildemo/controller/MailController.java
+++ email-demo/src/main/java/com/yd/zy/emaildemo/controller/MailController.java
@@ -3,7 +3,7 @@
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.FileSystemResource; 
 import org.springframework.lang.Nullable;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
